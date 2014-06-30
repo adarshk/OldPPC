698bbffb4dfe43d6117299ce155c7b459dd1efe9
Adarsh Kosuru <adarsh.kosuru@frogdesign.com>
2014-06-30 17:35:25 -0400

    First commit. Added harder and library search paths.

---
+++ PaperPixelCode/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  PaperPixelCode
+//
+//  Created by Adarsh Kosuru on 6/30/14.
+//  Copyright (c) 2014 Frog. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
